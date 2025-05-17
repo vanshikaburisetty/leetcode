a75d568bd3ab58ec62112d019a22927b284b1abc
vanshikaburisetty <138203521+vanshikaburisetty@users.noreply.github.com>
2025-05-17 12:09:32 +0530

    Time: 0 ms (100%), Space: 45.2 MB (87.08%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.java
+++ 0665-non-decreasing-array/0665-non-decreasing-array.java
@@ -1,18 +1,9 @@
 class Solution {
-    public boolean checkPossibility(int[] nums) {
-        int count = 0;         
-        //We can edit atmost 1 value, so break the loop if the count exceeds 1. 
-        for(int i = 1; i < nums.length && count<=1 ; i++){
-            //Checking prev value with curr value
-            if(nums[i-1] > nums[i]){
-                count++;
-                //checking the second prev value from curr value.
-                if(i-2<0 || nums[i-2] <= nums[i])
-                    nums[i-1] = nums[i];    //If prev value is > current val, change the prev value.
-                else 
-                    nums[i] = nums[i-1];    //If last 2 values both are > current, change the current value.
-            }
-        }
-        return count<=1; 
+    public boolean checkPossibility(int[] N) {
+        for (int i = 1, err = 0; i < N.length; i++)
+            if (N[i] < N[i-1])
+                if (err++ > 0 || (i > 1 && i < N.length - 1 && N[i-2] > N[i] && N[i+1] < N[i-1]))
+                    return false;
+        return true;
     }
 }
